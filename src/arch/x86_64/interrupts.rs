cfdadbaabaf5f0c7fb668053c0f213da5a935213
elfein <patlymlatun@gmail.com>
2021-11-06 23:31:03 -0700

    added breakpoint handler

--- src/arch/x86_64/interrupts.rs
+++ src/arch/x86_64/interrupts.rs
@@ -1,14 +1,20 @@
-
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::println;
+use lazy_static::lazy_static;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
 pub fn init_idt() {
-    let mut idt = InterruptDescriptorTable::new();
-    idt.breakpoint.set_handler_fn(breakpoint_handler);
+    IDT.load();
+
 }
 
-extern "x86-interrupt" fn breakpoint_handler(
-    stack_frame: InterruptStackFrame)
-{
-    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+lazy_static! {
+   static ref IDT: InterruptDescriptorTable = {
+      let mut idt = InterruptDescriptorTable::new();
+      idt.breakpoint.set_handler_fn(breakpoint_handler);
+      idt
+   };
+}
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
+   println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 // #![deny(warnings)]
+#![feature(abi_x86_interrupt)]
 #![feature(core_intrinsics, lang_items, llvm_asm)]
 // #![feature(alloc_error_handler)] // at the top of the file
 
