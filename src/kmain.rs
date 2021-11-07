e6e51b4b214116b78c957d901164c0bcb3b0d64d
Able <abl3theabove@gmail.com>
2021-11-08 00:47:41 +0300

    delete graphics holder

--- src/kmain.rs
+++ src/kmain.rs
@@ -14,16 +14,3 @@ pub extern "C" fn kernel_main() {
 
     crate::arch::shutdown();
 }
-fn graphics_holder() {
-    let buffer = [[(0, 0, 0); 1440]; 900];
-    /*
-    let _graphics_buffer = agl::GraphicsBufferHandle {
-        graphics_mode: agl::GModes::Vga800x600,
-        buffer_pointer: agl::BuffPoint::Single,
-        current_buff: buffer,
-        double_buff: buffer,
-        triple_buff: buffer,
-    };
-
-    */
-}
