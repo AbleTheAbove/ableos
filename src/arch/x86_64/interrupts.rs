37a8d67bb94b851ed392ee3b6e4e51f9cb92bf9d
elfein <patlymlatun@gmail.com>
2021-11-07 00:51:47 -0700

    changed back to release

--- src/arch/x86_64/interrupts.rs
+++ src/arch/x86_64/interrupts.rs
@@ -30,5 +30,6 @@ extern "x86-interrupt" fn page_fault_handler(
 extern "x86-interrupt" fn double_fault_handler(
    stack_frame: InterruptStackFrame, _error_code: u64) -> !
 {
+   println!["WHAT ARE YOU DOING"];
    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
