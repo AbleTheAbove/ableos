3c76b457ae0354d96a49fa6fda3315a586c86bae
Able <abl3theabove@gmail.com>
2021-11-08 17:58:40 +0300

    html

--- src/experiments/wm.rs
+++ src/experiments/wm.rs
@@ -5,13 +5,19 @@ pub struct Window {
     position: Point,
     fullscreen: bool,
 }
-
+// all of these should return a result
 impl Window {
-    pub fn fullscreen(&mut self) {
+    pub fn fullscreen(&mut self) -> Result<(), u8> {
         self.fullscreen = true;
+        Ok(())
     }
     pub fn revert_fullscreen(&mut self) {
         self.fullscreen = false;
     }
-    pub fn set_title(&mut self) {}
+    pub fn set_title(&mut self) {
+        todo!();
+    }
+    pub fn set_position(&mut self, pos: Point) {
+        self.position = pos;
+    }
 }
