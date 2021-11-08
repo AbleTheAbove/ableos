d8be4fdda208a454832681d576691b77a77166c9
Able <abl3theabove@gmail.com>
2021-11-08 04:17:55 +0300

    x86 branch

--- src/arch/x86_64/init.rs
+++ src/arch/x86_64/init.rs
@@ -5,9 +5,6 @@ use crate::println;
 
 pub fn init() {
     interrupts::init_idt();
-    // init_alloc();
     use core::fmt::Write;
     println!("Hello World{}", "!");
-
-    // vga::print_something();
 }
--- src/kmain.rs
+++ src/kmain.rs
@@ -8,11 +8,13 @@ pub extern "C" fn kernel_main() {
     GraphicsBuffer::draw();
     GraphicsBuffer::hide_cursor();
     GraphicsBuffer::show_cursor();
-    print!("Initialized");
+    println!("Initialized");
 
-    unsafe {
-        *(0xDEADBEEF as *mut u64) = 42;
+    fn h() {
+        println!("hi");
+        h()
     }
+    h();
 
     loop {}
 
