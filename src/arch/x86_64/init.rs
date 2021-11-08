b5536b38effce475f560870d6d7e4ab40817720e
elfein <patlymlatun@gmail.com>
2021-11-07 17:57:58 -0800

    added something- it was gdt stuff

--- src/arch/x86_64/init.rs
+++ src/arch/x86_64/init.rs
@@ -5,6 +5,5 @@ use crate::println;
 
 pub fn init() {
     interrupts::init_idt();
-    use core::fmt::Write;
     println!("Hello World{}", "!");
 }
--- src/arch/x86_64/interrupts.rs
+++ src/arch/x86_64/interrupts.rs
@@ -10,7 +10,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-        // idt.double_fault.set_handler_fn(double_fault_handler); // new
+        idt.double_fault.set_handler_fn(double_fault_handler); // new
         idt
     };
 }
