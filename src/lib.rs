2f26d674e8d7cdd1210c4b6a11181e809e95f7c7
elfein <patlymlatun@gmail.com>
2021-11-07 22:14:46 -0800

    added string value for RELEASE_TYPE in release builds

--- src/lib.rs
+++ src/lib.rs
@@ -44,4 +44,5 @@ pub const KERNEL_VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const RELEASE_TYPE: &str = "debug";
 #[cfg(not(debug_assertions))]
 /// A constant to check if the kernel is in release mode
+pub const RELEASE_TYPE: &str = "release";
 use relib::time;
