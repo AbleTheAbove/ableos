c2c0191b3b2aa55867bf71d5aae2d1287758ca7c
Able <abl3theabove@gmail.com>
2021-11-09 17:23:55 +0300

    working on CSPRNG

--- src/experiments/entropy.rs
+++ src/experiments/entropy.rs
@@ -1,1 +1,19 @@
-pub type EntropyPool = [u8; 128];
+pub struct Entropy {
+    // Everytime entropy is used decrement bits count
+    bits_count: u8,
+    pool: [u8; 128],
+}
+impl Entropy {
+    pub fn new() -> Self {
+        Self {
+            bits_count: 0,
+            pool: [0; 128],
+        }
+    }
+    pub fn poll_hardware() {
+        todo!();
+    }
+    pub fn read_bit() -> u8 {
+        1
+    }
+}
--- src/relib/math/rand/linearshift.rs
+++ src/relib/math/rand/linearshift.rs
@@ -4,7 +4,8 @@ pub struct LinearShiftRegister {
     reg: u64,
 }
 
-// 128 bit
+// 64 bit
+// non-cryptographically secure
 impl RNG for LinearShiftRegister {
     fn new() -> Self {
         Self { reg: (1 << 63) | 1 }
@@ -15,11 +16,18 @@ impl RNG for LinearShiftRegister {
         newbit
     }
     fn seed(&mut self, seed: u64) {
-        let shifted = (self.reg >> 1) ^ (self.reg >> 2) + 34;
-        let multitude = 1000000000000000;
+        let entropy = 34; // replace with hardware entropy
+        let shifted = (self.reg >> 1) ^ (self.reg >> 2) + entropy;
+        let x: u64 = 2983745;
+        let x123: u64 = 100000000;
+        let multitude: u64 = x123.wrapping_mul(x) / x;
         let mult = shifted.wrapping_mul(multitude);
-        crate::serial_print!("{}", mult);
         let seeded_bit = seed / mult;
+        if true {
+            crate::serial_println!("Entropy {}", entropy);
+            crate::serial_println!("Multitude {}", multitude);
+            crate::serial_println!("Seeded Bit {}", seeded_bit);
+        }
         for x in 0..seeded_bit {
             self.rand();
         }
