f3f914ff71c7b5f3bf46530a6b74b6c749d4b182
elfein <patlymlatun@gmail.com>
2021-11-10 17:42:42 -0800

    some struct :P

--- src/experiments/keymap.rs
+++ src/experiments/keymap.rs
@@ -1,9 +1,26 @@
 // This will be a way to map keys to other keys / keystrings / macros
 enum KeyType {
-    Char,
-    // SpecialKey
-    Shift,
-    Control,
+   Char,
+   // SpecialKey
+   Shift,
+   Control,
 }
 
 pub type KeyMap = [u8; 255];
+
+pub trait ScancodeSet {
+   /// Handles the state logic for the decoding of scan codes into key events.
+   fn advance_state(state: &mut DecodeState, code: u8) -> Result<Option<KeyEvent>, Error>;
+
+   /// Convert a Scan Code set X byte to our 'KeyCode' enum
+   fn map_scancode(code: u8) -> Result<KeyCode, Error>;
+
+   /// Convert a Scan Code Set X extended byte (prefixed E0) to our `KeyCode`
+   /// enum.
+   fn map_extended_scancode(code: u8) -> Result<KeyCode, Error>;
+}
+
+pub struct Layout {
+   single_byte: [KeyType; 256],
+   extended: [KeyType; 256],
+}
