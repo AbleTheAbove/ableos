f453b892c229e4f10cbb440532de54ff8b750d1c
Able <abl3theabove@gmail.com>
2021-11-12 21:37:16 -0600

    minimal things

--- src/panic.rs
+++ src/panic.rs
@@ -1,9 +1,9 @@
-use crate::{arch::sloop, println};
+use crate::{arch::sloop, println, serial_println};
 use core::{intrinsics::abort, panic::PanicInfo};
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-   println!("{}", info);
-
-   sloop()
+    println!("{}", info);
+    serial_println!("{}", info);
+    sloop()
 }
